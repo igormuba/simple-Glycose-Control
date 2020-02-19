ab6b2d8ace44679778c413f57acbbddb16f1b223
igormuba <igorm-uba@hotmail.com>
2020-02-19 14:26:30 -0300

    build script

---
+++ client/build/precache-manifest.94a69461e4f26a10ff7a7565ad548776.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d04c708d7e6ea17a99f6d3ea3433b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc6c8d36b2276b3b6e96",
+    "url": "/static/js/2.fca280c8.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.fca280c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b425130bcb5fad7003e",
+    "url": "/static/js/main.f330e693.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
